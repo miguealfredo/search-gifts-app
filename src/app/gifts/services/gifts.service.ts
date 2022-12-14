8a552415251c562a6c5673c4aec106e2d022ed4e
Miguel <migueldevelop@asdasd.com>
2022-12-14 15:31:10 -0600

    Add local storage results and refactor http request

--- src/app/gifts/services/gifts.service.ts
+++ src/app/gifts/services/gifts.service.ts
@@ -1,62 +1,93 @@
+import { query } from '@angular/animations';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Gift, SearchGiftsResponse } from '../interfeces/gitfs.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GiftsService {
-
-
-  apiKey = 'oXw8SZ7PRuQTI2uxQI4HhGuBPHPTit8Y'
-  private _historial: string[] = [];
   public results: Gift[] = [];
-
+  
   get historial(){
     return [...this._historial];
   }
 
+  private baseUrl:string = 'https://api.giphy.com/v1/gifs/search'
+  private apiKey:string = 'oXw8SZ7PRuQTI2uxQI4HhGuBPHPTit8Y'
+  private _historial: string[] = [];
+  private localStorageHistorial:string = 'historial'
+  private localStorageResult:string = 'results'
+  private giftsLimit:number = 10;
+  /**
+   * 
+   * @param httpClient 
+   */
   constructor(
     private httpClient:HttpClient
   ){
     
-      this._historial = JSON.parse( localStorage.getItem('historial')! ) || []
+    this._historial = JSON.parse( localStorage.getItem(this.localStorageHistorial)! ) || []
+    this.results = JSON.parse( localStorage.getItem(this.localStorageResult)! ) || []
   }
-
-  formatQuery = (query:string):string => query.trim().toLocaleLowerCase();
-
-  validateEmptyQuery = (query:string):boolean => query.length === 0
   
-  checkOnlyNineItems = ([...historial]:string[]):string[] => historial.splice(0,9);
-
-  validateRepetedNameItem = ([...historial]:string[],query:string):boolean => historial.includes(query)
-  
-  searchGifts(query:string=''):any{
+   /* 
+   * @param query 
+   * @returns 
+   */
+  private formatQuery = (query:string):string => query.trim().toLocaleLowerCase();
+  /**
+   * 
+   * @param query 
+   * @returns 
+   */
+  private validateEmptyQuery = (query:string):boolean => query.length === 0
+  /**
+   * 
+   * @param param0 
+   * @returns 
+   */
+  private checkOnlyNineItems = ([...historial]:string[]):string[] => historial.splice(0,9);
+  /**
+   * 
+   * @param param0 
+   * @param query 
+   * @returns 
+   */
+  private validateRepetedNameItem = ([...historial]:string[],query:string):boolean => historial.includes(query)
+  /**
+   * 
+   * @param query 
+   * @returns 
+   */
+  private getImages = (query:string):Observable<SearchGiftsResponse> => {
+    return this.httpClient.get<SearchGiftsResponse>(
+      this.baseUrl,
+      {
+        params:{
+          api_key:this.apiKey,
+          q:query,
+          limit:this.giftsLimit
+        }
+      }
+    )
+  }
+  /**
+   * 
+   * @param query 
+   * @returns 
+   */
+  public searchGifts(query:string=''):any{
     query = this.formatQuery(query);
     if(this.validateEmptyQuery(query)) return  null;
     if(this.validateRepetedNameItem(this._historial,query)) return null
     this._historial= this.checkOnlyNineItems(this._historial)
     this._historial.unshift(query);
-    localStorage.setItem('historial', JSON.stringify(this._historial))
-
+    localStorage.setItem(this.localStorageHistorial, JSON.stringify(this._historial))
+    this.getImages(query).subscribe( ( resp:SearchGiftsResponse ) => this.results = resp.data);
+    localStorage.setItem(this.localStorageResult, JSON.stringify(this.results))
     
-//?api_key=oXw8SZ7PRuQTI2uxQI4HhGuBPHPTit8Y&q=dragonm%20ball%20z&limit=10
-    this.httpClient.get<SearchGiftsResponse>(
-      'https://api.giphy.com/v1/gifs/search',
-      {
-        params:{
-          api_key:'oXw8SZ7PRuQTI2uxQI4HhGuBPHPTit8Y',
-          q:query,
-          limit:10
-        }
-      }
-      )
-    .subscribe(
-      ( resp:SearchGiftsResponse ) => {
-        console.log(resp.data);
-        this.results = resp.data;
-      }
-    )
   }
 
 
